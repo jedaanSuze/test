dcdb406407c6dda83e706d360cd700600c7c8074
Jedaan <jed3an@gmail.com>
2018-09-30 15:22:47 +0300

    test app

--- stagingdeploy.bat
+++ stagingdeploy.bat
@@ -1,4 +1,4 @@
 call gulp build --env=development
 cd dist
-call gcloud app deploy --project "wixlabs-plugins-dev" --verbosity="error" --no-stop-previous-version -v %1
+call gcloud app deploy --project "analog-patrol-217713" --verbosity="error" --no-stop-previous-version -v %1
 cd..
